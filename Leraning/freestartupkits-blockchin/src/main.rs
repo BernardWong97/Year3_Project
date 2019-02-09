13007321940a96b75b0247417a0ae714679b9320
mindaugas <my>
2019-02-09 03:37:51 +0000

    ADD: freestartupkits-blockchin

---
+++ Leraning/freestartupkits-blockchin/src/main.rs
@@ -0,0 +1,37 @@
+//!
+//! Implementation of block-chain from:
+//! https://freestartupkits.com/articles/technology/cryptocurrency-news-and-tips/ultimate-rust-blockchain-tutorial/
+//!
+
+
+// this would be the library crate
+mod rusty_chain;
+
+use std::process;
+
+//use rusty_chain::blockchain::Blockchain;
+//use rusty_chain::error::MiningError;
+use crate::rusty_chain::MiningError;
+use crate::rusty_chain::BlockChain;
+
+fn main() {
+    println!("Welcome to Rusty Chain");
+
+    run().
+        unwrap_or_else(|e| {
+            println!("Error: {}", e);
+            process::exit(1)
+        })
+}
+
+fn run() -> Result<(), MiningError> {
+    let mut chain = BlockChain::new()?;
+    println!("Send 1 RC to foo");
+    chain.add_block("cool block bro!")?;
+
+    println!("Traversing blockchain:\n");
+    chain.traverse();
+
+    Ok(())
+}
+
