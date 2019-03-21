8cc7155fd055e797c9ad9fe4c5e7dbb0035426f7
Mindaugas Sharskus <g00339629@gmit.ie>
2019-03-21 23:50:13 +0000

    Finished CLI tutorial

--- Learning/minigrep/src/main.rs
+++ Learning/minigrep/src/main.rs
@@ -1,28 +1,27 @@
-///
-/// Mini grep from tutorial
-/// https://doc.rust-lang.org/stable/book/ch12-01-accepting-command-line-arguments.html
-///
+//!
+//! Mini grep from tutorial
+//! https://doc.rust-lang.org/stable/book/ch12-01-accepting-command-line-arguments.html
+//!
+//! to run it:
+//!     `cargo run <query> <file>`
+//!     `CASE_INSENSITIVE cargo run <query> <filename>`
+//!
 
 use std::{env, process};
 
 use minigrep;
 use minigrep::Config;
 
-
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
 
     let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);    // print to stderr
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config){
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);  // print to stderr
         process::exit(1);
     }
 }
-
-
-
-
