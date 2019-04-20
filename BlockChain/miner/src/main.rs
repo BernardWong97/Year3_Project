4d5d94cffc77d4d37912365f4c7a80d07960b2b3
Mindaugas Sharskus <g00339629@gmit.ie>
2019-04-20 16:13:03 +0100

    refactoring: +documentation

--- BlockChain/miner/src/main.rs
+++ BlockChain/miner/src/main.rs
@@ -1,3 +1,15 @@
+//!
+//! Miner API for controlling miner using HTTP request.
+//!
+//! Mindaugas Sharskus
+//! 20-04-2019
+//!
+//! ToDo (feature improvements):
+//! - Allow multi hash mining (queue jobs)
+//! - Allow for checking for mining status instead of waiting until its done.
+//! This will be needed for greater difficulty. Idea is: firs summit a job,
+//! then at separate request check if job is done (hash is mined).
+
 
 #![feature(proc_macro_hygiene, decl_macro)]
 
@@ -6,30 +18,16 @@
 #[macro_use] extern crate serde_derive;
 
 use rocket::{Request, response::content, data::Data};
-
-use miner::Miner;
-use block_chain::block_header::BlockHeader;
-use std::io::Read;
 use rocket_contrib::json::{JsonValue, Json};
+use std::io::Read;
+
+use block_chain::block_header::BlockHeader;
+use miner::Miner;
 
 
-#[get("/hello")]
-fn world() -> &'static str {
-    "Hello, world!"
-}
-
-#[get("/start/<difficulty>")]
-fn random(difficulty: usize) -> JsonValue{
-    let mut header = BlockHeader::first();
-    header.set_difficulty(difficulty);
-    let nonce = Miner::new(&header).start();
-
-    json!({
-        "status":"ok",
-        "nonce":nonce
-    })
-}
-
+///
+/// Mines hash for a given `BlockHeader`.
+///
 #[post("/start", format = "application/json", data = "<block_header>")]
 fn get_hash(block_header: Json<BlockHeader>) -> JsonValue {
 
@@ -42,9 +40,41 @@ fn get_hash(block_header: Json<BlockHeader>) -> JsonValue {
     })
 }
 
-fn main() {
+///
+/// Mines dummy hash for the given difficulty (testing).
+///
+#[get("/start/<difficulty>")]
+fn random(difficulty: usize) -> JsonValue{
+    let mut header = BlockHeader::first();
+    header.set_difficulty(difficulty);
+    let nonce = Miner::new(&header).start();
+
+    json!({
+        "status":"ok",
+        "nonce":nonce
+    })
+}
+
+///
+/// Says hello.
+///
+#[get("/")]
+fn world() -> &'static str {
+    "Miner says: Hello!"
+}
+
+///
+/// builds "Rocket"
+///
+fn rocket() -> rocket::Rocket {
     rocket::ignite()
         .mount("/miner",
                routes![world, get_hash, random])
-        .launch();
+}
+
+///
+/// Launches "Rocket"
+///
+fn main() {
+    rocket().launch();
 }
